8ec743363144fa8825a5dffd13e92f60aa2b35e4
taro588 <105845938+taro588@users.noreply.github.com>
2026-09-25 21:56:57 +0800

    feat: add Painter Qt compatibility layer

---
+++ plugin/core/qt_compat.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+import substance_painter
+
+
+def qt_modules():
+    version = substance_painter.application.version_info()
+    if version < (10, 1, 0):
+        from PySide2 import QtCore, QtWidgets
+    else:
+        from PySide6 import QtCore, QtWidgets
+    return QtCore, QtWidgets
