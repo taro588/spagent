702b2f45dce41a707c80f96a706b5e1fb969bd83
taro588 <105845938+taro588@users.noreply.github.com>
2026-09-26 23:00:34 +0800

    release: bump to 0.3.3 after real-machine compatibility fix

--- plugin/sp_ai_assistant.py
+++ plugin/sp_ai_assistant.py
@@ -12,7 +12,7 @@ from core.self_check import run_self_check
 from ui.chat_dock import ChatDock
 
 _widgets = []
-PLUGIN_VERSION = "0.3.2"
+PLUGIN_VERSION = "0.3.3"
 MIN_PAINTER_VERSION = (7, 2, 0)
 
 
