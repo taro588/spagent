78e6f8f2d238b09a20efd25d4857fb88abf5efc9
taro588 <105845938+taro588@users.noreply.github.com>
2026-09-26 23:43:21 +0800

    feat: expose QtGui for chat image previews

--- plugin/core/qt_compat.py
+++ plugin/core/qt_compat.py
@@ -6,7 +6,7 @@ import substance_painter
 def qt_modules():
     version = substance_painter.application.version_info()
     if version < (10, 1, 0):
-        from PySide2 import QtCore, QtWidgets
+        from PySide2 import QtCore, QtGui, QtWidgets
     else:
-        from PySide6 import QtCore, QtWidgets
-    return QtCore, QtWidgets
+        from PySide6 import QtCore, QtGui, QtWidgets
+    return QtCore, QtGui, QtWidgets
